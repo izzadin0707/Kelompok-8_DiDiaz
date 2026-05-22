f5044d8b28885b98f217f7aac30bcb48869fb6f9
Dinzadin <153895119+Dinzadin@users.noreply.github.com>
2026-05-22 21:14:57 +0700

    modification router healper;

--- helper.js
+++ helper.js
@@ -1,5 +1,20 @@
+function navigate(path) {
+    location.hash = `/${path}`;
+}
+
+function router() {
+    const page = location.hash.substring(2);
+    console.log(page);
+    loadPage(page);
+    updateSidebarActive();
+}
+
 function loadPage(page) {
-    $("#content").load(`pages/${page}.html`);
+    $("#content").load(`pages/${page}/index.html`, function (response, status) {
+        if (status === "error") {
+            $("#content").load("pages/404/index.html");
+        }
+    });
 }
 
 function updateSidebarActive() {
